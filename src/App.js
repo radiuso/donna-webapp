dc6af1e617fc407f33dc27f8071888554ec5ff98
radiusario <radiusario@gmail.com>
2018-01-06 17:34:27 +0100

    add spectre.css and use sass

--- src/App.js
+++ src/App.js
@@ -5,14 +5,11 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
+      <div className="empty">
+        <div className="empty-icon">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Donna</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        </div>
+        <p className="empty-title h5">Welcome to Donna</p>
       </div>
     );
   }
