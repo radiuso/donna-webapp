e5257c120fcc6a6df2f4826fb56666df55d15956
radiusario <radiusario@gmail.com>
2018-01-06 20:23:14 +0100

    Fetch version from graphql api

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,23 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 import {Helmet} from 'react-helmet';
+import {graphql} from 'react-apollo';
+import gql from 'graphql-tag';
 
 import logo from '../logo.svg';
 
-const Home = () => (
-  <div>
-    <Helmet>
-      <title>Home</title>
-    </Helmet>
-    <div className="empty">
-      <div className="empty-icon">
-        <img src={logo} className="App-logo" alt="logo"/>
+class Home extends Component {
+  render() {
+    const { version } = this.props.data;
+    return (
+      <div>
+        <Helmet>
+          <title>Home</title>
+        </Helmet>
+        <div className="empty">
+          <div className="empty-icon">
+            <img src={logo} className="App-logo" alt="logo"/>
+          </div>
+          <p className="empty-title h5">Welcome to Donna (using api v{version})</p>
+          <p className="empty-subtitle">Please login to enjoy</p>
+          <Link to="/auth/login" className="btn">Login</Link>
+        </div>
       </div>
-      <p className="empty-title h5">Welcome to Donna</p>
-      <p className="empty-subtitle">Please login to enjoy</p>
-      <Link to="/auth/login" className="btn">Login</Link>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
-export default Home;
+export default graphql(gql`query { version }`)(Home);
--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
+import {ApolloProvider} from 'react-apollo';
+import {ApolloClient} from 'apollo-client';
+import {HttpLink} from 'apollo-link-http';
+import {InMemoryCache} from 'apollo-cache-inmemory';
+
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+const client = new ApolloClient({
+  link: new HttpLink({uri: 'http://localhost:8080/graphql'}),
+  cache: new InMemoryCache()
+});
+
 ReactDOM.render(
   <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root')
-);
+  <ApolloProvider client={client}>
+    <App/>
+  </ApolloProvider>
+</BrowserRouter>, document.getElementById('root'));
 
 registerServiceWorker();
