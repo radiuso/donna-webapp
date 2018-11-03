5f9747dde26707590133a109318d151fbe8701f9
Thomas <tvandermeersch@alteca.fr>
2018-11-03 11:46:33 +0100

    header as pure component

--- src/layout/Header/Header.jsx
+++ src/layout/Header/Header.jsx
@@ -1,17 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
-class Header extends Component {
-  render() {
-    return (
-      <nav className="navbar is-primary" role="navigation" aria-label="main navigation">
-        <div className="navbar-brand">
-          <Link to="/" className="navbar-item">Donna</Link>
-        </div>
+const Header = _ => (
+  <nav className="navbar is-primary" role="navigation" aria-label="main navigation">
+    <div className="navbar-brand">
+      <Link to="/" className="navbar-item">Donna</Link>
+    </div>
+    <div className="navbar-menu">
+      <div class="navbar-start">
         <Link to="/customers" className="navbar-item">Clients</Link>
-      </nav>
-    );
-  }
-}
+      </div>
+    </div>
+  </nav>
+);
 
 export default Header;
