588b7da3d5369334a5d6a8f72d89ebb6d88e3f3b
Thomas <tvandermeersch@alteca.fr>
2018-11-13 13:47:53 +0100

    update view

--- src/views/Orders/OrdersListView.jsx
+++ src/views/Orders/OrdersListView.jsx
@@ -9,24 +9,30 @@ class OrdersListView extends Component {
 
     render() {
         return (
-            <div className="navbar level">
-                <div className="level-item has-text-centered">
-                    { format(this.state.currentDate, 'MM/DD/YYYY') }
-                    <button className="button">
-                        <Icon icon="calendar-alt" />
-                    </button>
-                </div>
-                <div className="level-right">
-                    <div className="level-item">
-                        <button className="button">
-                            <Icon icon="search" />
-                        </button>
-                        <button className="button">
-                            <Icon icon="plus" />
-                        </button>
+            <React.Fragment>
+                <div className="navbar level">
+                    <div className="level-left">
+                        <div className="level-item has-text-centered subtitle is-3">
+                            <span className="has-text-primary has-margin-right-10">
+                                { format(this.state.currentDate, 'MM/DD/YYYY') }
+                            </span>
+                            <button className="button is-primary is-inverted">
+                                <Icon icon="calendar-alt" />
+                            </button>
+                        </div>
+                    </div>
+                    <div className="level-right">
+                        <div className="level-item has-text-centered subtitle is-3">
+                            <button className="button is-text is-primary is-inverted">
+                                <Icon icon="search" />
+                            </button>
+                            <button className="button is-text is-primary is-inverted">
+                                <Icon icon="plus" />
+                            </button>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </React.Fragment>
         );
     }
 }
